81578aad2ccd235c8b03e78a0ee7136d268b74ae
Alexander <al.x.falconer@gmail.com>
2020-12-11 12:22:34 -0500

    commit tree 89d30a293c55d01ba743e460b925c48e3db8d3b6

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,30 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
-
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
+    puts ("Happy Birthday #{kids_name}! You are now #{age} years old!")
+  end
+end
+
+def age_appropriate_birthday(birthday_kids)
+  younger_birthday = {}
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+      younger_birthday[kids_name] = age
+      puts ("Happy Birthday #{kids_name}! You are now #{age} years old!")
+    else
+      puts "You are too old for this."
+    end
+  end
+end
+
+def select_winner(passengers)
+  winner = ""
+  passengers.each do |suite, name|
+    if suite == :suite_a &&
+      name.start_with?("A")
+      winner = name
+    end
+  end
+  winner
 end
 
 
